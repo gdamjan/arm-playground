5e2077f354c4492cb209ff068456c39d01b4cf85
Дамјан Георгиевски <gdamjan@gmail.com>
2018-02-24 04:25:44 +0100

    simplest C program that blinks

---
+++ 02_plain_c/README.md
@@ -0,0 +1,3 @@
+a simple program in C that …. the linker script is set to run in ram.
+
+run `make stlink` in one terminal, and then `make gdb` to load the program in ram. use `s` to step through instructions.
---
+++ 02_plain_c/simple.c
@@ -0,0 +1,35 @@
+__attribute__ ((weak, naked))
+void custom_main (void) {
+    (*(volatile unsigned int *)(0x40021018)) |= (1 << 4);
+
+    (*(volatile unsigned int *)(0x40011004)) |= (0x00 << (((13 - 8) * 4) + 2));
+    (*(volatile unsigned int *)(0x40011004)) |= (0x02 << ((13 - 8) * 4));
+
+    while(1) {
+        (*(volatile unsigned int *)(0x40011010)) = (1 << 13);
+        for (int i = 0; i < 1000000; ++i) __asm__("nop");
+
+        (*(volatile unsigned short *)(0x40011014)) = (1 << 13);
+        for (int i = 0; i < 500000; ++i) __asm__("nop");
+    }
+}
+
+__attribute__ ((section(".vectors")))
+struct {
+    unsigned int *initial_sp_value;
+    void (*reset)(void);
+    void (*nmi)(void);
+    void (*hard_fault)(void);
+    void (*memory_manage_fault)(void);
+    void (*bus_fault)(void);
+    void (*usage_fault)(void);
+    void (*reserved_x001c[4])(void);
+    void (*sv_call)(void);
+    void (*debug_monitor)(void);
+    void (*reserved_x0034)(void);
+    void (*pend_sv)(void);
+    void (*systick)(void);
+    void (*irq[68])(void);
+} vector_table = {
+    .reset = custom_main,
+};
